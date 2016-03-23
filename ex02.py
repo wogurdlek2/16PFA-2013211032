ea16012ee595c3c18c5193b09cee1ffa947172fe
CAD Client <CAD Client>
2016-03-23 18:18:29 +0900

    fix

--- ex02.py
+++ ex02.py
@@ -1,21 +1,9 @@
-print "Hens", 25 + 30 / 6
-print "Roosters", 100 - 25 * 3 % 4
-#comment shit i dont know %
-print "Now I will count the eggs:"
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
 
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print "I could have code like this." # and the comment after is ignored
 
-print "Is it true that 3 + 2 < 5 - 7?"
+# You can also use a comment to "disable" or comment out a piece of code:
+# print "This won't run."
 
-print 3 + 2 < 5 - 7
-
-print "What is 3 + 2?", 3 + 2
-print "What is 5 - 7?", 5 - 7.0
-
-print "Oh, that's why it's False."
-
-print "How about some more."
-
-print "Is it greater?", 5 > -2
-print "Is it greater or equal?", 5 >= -2
-#-*-coding:cp949
+print "This will run."
